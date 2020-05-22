c9fbe1b92a70e5452b1c016b701a9813d6d2fbc7
mamathav <mamatha.veer@britehouse.co.za>
2020-05-22 23:28:54 +0200

    Adding Rates controller to the solution.

---
+++ API/LiveRates/LiveRates/Controllers/RatesController.cs
@@ -0,0 +1,44 @@
+﻿using LiveRates.DataStorage;
+using LiveRates.HubConfig;
+using LiveRates.TimerFeatures;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LiveRates.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RatesController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private IHubContext<RatesHub> _hub;
+        public RatesController(IHubContext<RatesHub> hub)
+        {
+            _hub = hub;
+        }
+        // GET: api/RateChanges
+        [EnableCors("Policy1")]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                var timerManager = new TimerManager(() => _hub.Clients.All.SendAsync("transferRatesData", DataManager.GetData()));
+
+                return Ok(new { Message = "Request Completed" });
+
+            }
+            catch(Exception ex)
+            {
+               return BadRequest("Three requests per hour limit has reaced");
+            }
+
+        }
+
+    }
+}
